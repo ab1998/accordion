576a941f3a3b033feb7eb49fd0e135a49db5fbca
ab1998 <abhinavsharma.ddn@gmail.com>
2024-04-28 14:40:30 +0530

    Changes in accordion

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { data } from './constants/constants'
 
 function App() {
-  const [count, setCount] = useState(0)
+const[selected,setSelected] = useState(null)
 
+const toggle = (i)=>{
+if(selected===i){
+  return setSelected(null)
+}
+setSelected(i)
+}
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="wrapper">
+        <div className="accordion">
+          {data.map((data,i) => (
+          <div className="item">
+            <div className="title" onClick={()=>toggle(i)}>
+              <h2>{data.question}</h2>
+              <span>{selected===i?"-":"+"}</span>
+            </div>
+            <div className={selected===i?"contentshow":"content"}>
+              {data.answer}
+              </div>
+              </div>
+        ))}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
